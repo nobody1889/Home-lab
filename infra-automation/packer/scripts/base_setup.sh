9f6129bce46c82eb79dae5d9b9b3b99e55093c14
nobody1889 <nobodyisherefornow1889@gmail.com>
2026-09-06 15:00:15 +0330

    update

--- infra-automation/packer/scripts/base_setup.sh
+++ infra-automation/packer/scripts/base_setup.sh
@@ -4,19 +4,12 @@ set -e
 apt-get update
 apt-get upgrade -y
 
-apt-get install -y \
-    curl \
-    wget \
-    git \
-    vim \
-    htop \
-    net-tools \
-    jq \
-    ca-certificates \
-    apt-transport-https software-properties-common \
-    qemu-guest-agent cloud-init cloud-guest-utils \
-    sudo openssh-server unattended-upgrades
-
+apt-get -y install \
+  curl wget gnupg2 ca-certificates \
+  apt-transport-https \
+  qemu-guest-agent cloud-init cloud-guest-utils \
+  sudo openssh-server unattended-upgrades
+  
 systemctl enable qemu-guest-agent
 systemctl enable ssh
 systemctl enable cloud-init
