26e432da260c1de372ff00ed62084c39bd7c2592
nobody1889 <nobodyisherefornow1889@gmail.com>
2026-09-06 15:22:03 +0330

    update: we handel it on seed so we remove the enable cloud-init

--- infra-automation/packer/scripts/base_setup.sh
+++ infra-automation/packer/scripts/base_setup.sh
@@ -9,7 +9,8 @@ apt-get -y install \
   apt-transport-https \
   qemu-guest-agent cloud-init cloud-guest-utils \
   sudo openssh-server unattended-upgrades
-  
+
 systemctl enable qemu-guest-agent
 systemctl enable ssh
-systemctl enable cloud-init
+
+echo "✅ base setup complete"
